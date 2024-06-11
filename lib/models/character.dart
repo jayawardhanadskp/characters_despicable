36244f6e5a1ca69db784096d83ae818b37b496d2
jayawardhanadskp <kasunpradeep.ds@gmail.com>
2024-06-11 20:37:04 +0530

    characters

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -22,6 +22,6 @@ List characters = [
       imagePath: "assets/images/Agnes_gru.png",
       description:
       "Agnes Gru it is one of Gru and Lucy's three adopted daughters, alongside her sisters Margo and Edith. She is the youngest child of the three sisters. She greatly adores unicorns, as shown on various occasions. Agnes is a little girl with dark brown eyes. Her long black hair is tied in an upwards ponytail with a red scrunchie. Most of the time, Agnes wears blue overalls over a yellow and brown striped t-shirt, and white sneakers with yellow socks. She also wears a white ballet outfit like Edith and Margo (at the ballet recital). For pajamas, Agnes wears a long blue nightshirt covered with teddy bears and polar bear slippers; her hair stays the same. On her birthday, Agnes is wearing a dress that resembles a princess riding a unicorn. The colors are similar to her regular outfit. She also wears a blue princess hat on her head.",
-      colors: [Colors.pink.shade200, Colors.redAccent.shade400]
+      colors: [Colors.pink.shade200, Colors.redAccent.shade400] 
   ),
 ];
